6b78acef13e037a75a0f494d8323f6c98e7dfa6c
Nicolas Ehrhardt <nico.ehrhardt@gmail.com>
2026-01-30 09:15:57 -0800

    Remove sleep delay

--- main.py
+++ main.py
@@ -107,7 +107,6 @@ def main():
     fig1 = build_figs_for_params(params[0], gc)
     
     print("Waiting before generating Figure 2...")
-    time.sleep(15) # Add a substantial delay between generating the first and second figure
     
     print("Generating Figure 2...")
     fig2 = build_figs_for_params(params[1], gc)
